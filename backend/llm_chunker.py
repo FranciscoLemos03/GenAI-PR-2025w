db8e801d0d403c97cc7588b645e1a00d6eda2462
Francisco Lemos <56840967+FranciscoLemos03@users.noreply.github.com>
2026-01-17 17:34:04 +0100

    Quick fix
    fix, error .env call

--- backend/llm_chunker.py
+++ backend/llm_chunker.py
@@ -6,10 +6,15 @@ import time
 import re
 import uuid
 from embedder import Embedder
+from dotenv import load_dotenv
+from pathlib import Path
 
 # =========================
 # CONFIG
 # =========================
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 API_KEY = os.getenv("GEMMA_API_KEY")
 
